34ed354404e2283ac78791d2bb1be69162a0b6e1
Tiezheng Yuan <tiezheng.yuan@nih.gov>
2023-07-12 09:24:05 -0400

    rebase

--- Smartscope/server/main/asgi.py
+++ Smartscope/server/main/asgi.py
@@ -16,4 +16,19 @@ django.setup()
 
 from channels.routing import ProtocolTypeRouter
 from django.core.asgi import get_asgi_application
+<<<<<<< HEAD
 from Smartscope.server.websocket.routing import application
+=======
+from channels.security.websocket import AllowedHostsOriginValidator, OriginValidator
+from channels.auth import AuthMiddlewareStack
+from Smartscope.server.websocket.routing import router
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AllowedHostsOriginValidator(
+        AuthMiddlewareStack(
+            router
+        )
+    )
+})
+>>>>>>> 65a9bcc (clarify asgi application and fix websocket)
